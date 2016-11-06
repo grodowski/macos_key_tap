313132cf0f80de8c25b8090c155ff254f3dae486
Jan <jgrodowski@gmail.com>
2016-11-06 13:30:40 +0100

    Initial Commit

---
+++ KeyPlaygroundObjc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KeyPlaygroundObjc
+//
+//  Created by Jan Grodowski on 06/11/2016.
+//  Copyright © 2016 Jan Grodowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
