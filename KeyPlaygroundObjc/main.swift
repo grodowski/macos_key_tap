54eeac408083ac47a87188045dbf6ac7e54428cc
Jan <jgrodowski@gmail.com>
2016-11-06 15:41:14 +0100

    initial commit

--- KeyPlaygroundObjc/main.swift
+++ KeyPlaygroundObjc/main.swift
@@ -5,8 +5,50 @@
 //  Created by Jan Grodowski on 06/11/2016.
 //  Copyright © 2016 Jan Grodowski. All rights reserved.
 //
+// capturing keys with sudo: http://stackoverflow.com/a/31898592/3526316
+// posting key events: http://stackoverflow.com/a/25070476/3526316
+//
+// This proof of concept runs as root and captures system wide keyboard events.
+// Additionally, it will print 😱 to the key event queue every second. Amazing.
 
 import Foundation
 
-print("Hello, World!")
+func dispatchEvent(chars: Array<UniChar>) {
+    let src = CGEventSource(stateID: CGEventSourceStateID.hidSystemState)
+    guard let evt = CGEvent(keyboardEventSource: src, virtualKey: 0x52, keyDown: true) else { // 0
+        print("Unable to initialize CGEvent")
+        exit(1)
+    }
+    evt.keyboardSetUnicodeString(stringLength: 2, unicodeString: chars) // Override virtualKey
+    evt.post(tap: CGEventTapLocation.cghidEventTap)
+}
 
+func myCGEventCallback(proxy: CGEventTapProxy, type: CGEventType, event: CGEvent, refcon: UnsafeMutableRawPointer?) -> Unmanaged<CGEvent>? {
+    print("Key event: ", event.getIntegerValueField(.keyboardEventKeycode))
+    return Unmanaged.passRetained(event)
+}
+
+DispatchQueue.global(qos: .background).async {
+    print("Starting background listener... 🔊➡️👂") // Some day it will receive events from the remote keyboard
+    let charArray = Array("😱".utf16)
+    while (true) {
+        dispatchEvent(chars: charArray)
+        sleep(1)
+    }
+}
+
+let eventMask = (1 << CGEventType.keyDown.rawValue) | (1 << CGEventType.keyUp.rawValue)
+guard let eventTap = CGEvent.tapCreate(tap: .cgSessionEventTap,
+                                       place: .headInsertEventTap,
+                                       options: .defaultTap,
+                                       eventsOfInterest: CGEventMask(eventMask),
+                                       callback: myCGEventCallback,
+                                       userInfo: nil) else {
+                                        print("Failed to create event tap, do you have root access? 🔑❓")
+                                        exit(1)
+}
+
+let runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0)
+CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, .commonModes)
+CGEvent.tapEnable(tap: eventTap, enable: true)
+CFRunLoopRun()
